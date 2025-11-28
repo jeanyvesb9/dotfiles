bee6efdd9c1d1e07e98240055dd56de15267a567
Jean Yves Beaucamp <jean.yves.beaucamp@cern.ch>
2025-11-28 04:21:34 +0100

    Remove overrides for ssh

--- home/dot_local/share/dotfiles/cern/lxp_paths_setup.zsh
+++ home/dot_local/share/dotfiles/cern/lxp_paths_setup.zsh
@@ -16,8 +16,4 @@ elif [[ ($ID == "rhel" || $ID == "almalinux") && $VERSION_ID == 9* ]]; then
     export PATH="$HOME/.local/el9/bin:$PATH"
     export LD_LIBRARY_PATH="$HOME/.local/el9/lib:$HOME/.local/el9/lib64:$LD_LIBRARY_PATH"
     export LD_RUN_PATH="$HOME/.local/el9/lib:$HOME/.local/el9/lib64:$LD_RUN_PATH"
-
-    # TODO: Is this still needed?
-    alias ssh='ssh -o RequiredRSASize=1024'
-    alias scp='scp -o RequiredRSASize=1024'
 fi
