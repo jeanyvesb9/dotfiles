bd424474db19b4862bd4ea9289b0f5190f0c6be4
Jean Yves Beaucamp <jean.yves.beaucamp@cern.ch>
2025-11-28 04:24:22 +0100

    Simplify os-specific local paths config

--- home/dot_local/share/dotfiles/cern/lxp_paths_setup.zsh
+++ home/dot_local/share/dotfiles/cern/lxp_paths_setup.zsh
@@ -8,12 +8,15 @@ fi
 
 # OS-Specific configuration
 . /etc/os-release
+os_local_path=""
 if [[ $ID == "centos" && $VERSION_ID == 7* ]]; then
-    export PATH="$HOME/.local/centos7/bin:$PATH"
-    export LD_LIBRARY_PATH="$HOME/.local/centos7/lib:$HOME/.local/centos7/lib64:$LD_LIBRARY_PATH"
-    export LD_RUN_PATH="$HOME/.local/centos7/lib:$HOME/.local/centos7/lib64:$LD_RUN_PATH"
+    os_local_path="centos7"
 elif [[ ($ID == "rhel" || $ID == "almalinux") && $VERSION_ID == 9* ]]; then
-    export PATH="$HOME/.local/el9/bin:$PATH"
-    export LD_LIBRARY_PATH="$HOME/.local/el9/lib:$HOME/.local/el9/lib64:$LD_LIBRARY_PATH"
-    export LD_RUN_PATH="$HOME/.local/el9/lib:$HOME/.local/el9/lib64:$LD_RUN_PATH"
+    os_local_path="el9"
+fi
+
+if [[ ! -z "$os_local_path" ]]; then
+    export PATH="$HOME/.local/${os_local_path}/bin:$PATH"
+    export LD_LIBRARY_PATH="$HOME/.local/${os_local_path}/lib:$HOME/.local/${os_local_path}/lib64:$LD_LIBRARY_PATH"
+    export LD_RUN_PATH="$HOME/.local/${os_local_path}/lib:$HOME/.local/${os_local_path}/lib64:$LD_RUN_PATH"
 fi
