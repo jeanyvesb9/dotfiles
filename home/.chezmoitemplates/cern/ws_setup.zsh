0d6f7389fe0a37ba38231924324ef5131b3542c5
Jean Yves Beaucamp <jean.yves.beaucamp@cern.ch>
2025-12-17 13:09:36 -0300

    Update CERN ws setup instructions

--- home/.chezmoitemplates/cern/ws_setup.zsh
+++ home/.chezmoitemplates/cern/ws_setup.zsh
@@ -11,15 +11,25 @@ export PATH="$HOME/.local/bin/cern_ws:$PATH"
 #
 # $ gpg --full-generate-key # Use all the default options
 #
+# If you're using a YubiKey, follow the instructions in 
+# https://support.yubico.com/s/article/Using-Your-YubiKey-with-OpenPGP
+# to generate a key locked by your 2FA hardware key.
+#
 # Now, get the uid for your created key from
+#
 # $ gpg --list-keys
+#
 # and use that uid to initialize the pass keychain storage, e.g.
+#
 # $ pass init "Jean Yves Beaucamp <jeanyvesb9@gmail.com>"
 #
 # Finally, insert the secret otpauth SHA1 keys for the CERN accounts
+#
 # $ pass otp insert cern-jbeaucam <otpauth-jbeaucam>
 # $ pass otp insert cern-jyb <otpauth-jyb>
+#
 # The keys can be found in ~/Dropbox/cern/private/otp_secret_keys.txt
+
 alias otp-jbeaucam="pass otp cern-jbeaucam"
 alias otp-jyb="pass otp cern-jyb"
 
