fd211c037e1d2e36072be474624b59bfe099a50b
Jean Yves Beaucamp <jeanyvesb9@gmail.com>
2026-01-15 22:45:31 -0300

    Move lxp scripts and configuration instructions to a separate generic directory (lxp-session-manager)

--- home/.chezmoitemplates/cern/ws_setup.zsh
+++ home/.chezmoitemplates/cern/ws_setup.zsh
@@ -5,33 +5,8 @@ export CERN_USERNAME="jbeaucam"
 # Add CERN-specific scripts
 export PATH="$HOME/.local/bin/cern_ws:$PATH"
 
-# Get OTP passwords
-# This uses pass-otp (https://github.com/tadfisher/pass-otp)
-# Before running the first time, you'll need to have a GPG secret key:
-#
-# $ gpg --full-generate-key # Use all the default options
-#
-# If you're using a YubiKey, follow the instructions in 
-# https://support.yubico.com/s/article/Using-Your-YubiKey-with-OpenPGP
-# to generate a key locked by your 2FA hardware key.
-#
-# Now, get the uid for your created key from
-#
-# $ gpg --list-keys
-#
-# and use that uid to initialize the pass keychain storage, e.g.
-#
-# $ pass init "Jean Yves Beaucamp <jeanyvesb9@gmail.com>"
-#
-# Finally, insert the secret otpauth SHA1 keys for the CERN accounts
-#
-# $ pass otp insert cern-jbeaucam <otpauth-jbeaucam>
-# $ pass otp insert cern-jyb <otpauth-jyb>
-#
-# The keys can be found in ~/Dropbox/cern/private/otp_secret_keys.txt
-
-alias otp-jbeaucam="pass otp cern-jbeaucam"
-alias otp-jyb="pass otp cern-jyb"
+# LXPlus session manager scripts and aliases
+export PATH="$HOME/.local/bin/lxp-session-manager:$PATH"
 
 alias lxtunnel="lxp lxtunnel"
 alias lxtunnelcm="lxp --cm lxtunnel"
@@ -39,6 +14,11 @@ alias lxtunnelcme="lxpc --cm-exit lxtunnel"
 
 alias lxproxy="lxp -p"
 alias lxvnc="lxp -v"
+
+# The GPG otpauth master keys can be found in ~/Dropbox/cern/private/otp_secret_keys.txt
+# The installation instructions are in the lxp-session-manager README.md
+alias otp-jbeaucam="pass otp cern-jbeaucam"
+alias otp-jyb="pass otp cern-jyb"
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
